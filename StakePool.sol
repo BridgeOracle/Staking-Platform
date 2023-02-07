92c87951d0d9a0bd4daf202764afc3287cb4845b
BrgOracle <122056710+BrgOracle@users.noreply.github.com>
2023-02-07 16:07:00 +0330

    Create StakePool.sol

--- StakePool.sol
+++ StakePool.sol
@@ -320,26 +320,6 @@ library SafeMath {
 }
 
 /**
- * @title Various utilities useful for uint256.
- */
-library UInt256Lib {
-
-    uint256 private constant MAX_INT256 = ~(uint256(1) << 255);
-
-    /**
-     * @dev Safely converts a uint256 to an int256.
-     */
-    function toInt256Safe(uint256 a)
-    internal
-    pure
-    returns (int256)
-    {
-        require(a <= MAX_INT256);
-        return int256(a);
-    }
-}
-
-/**
  * @dev Collection of functions related to the address type
  */
 library Address {
